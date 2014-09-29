ef66f3bd662244d67c0c84dc9c909b3b7b8ce0f9
shearer12345 <shearer12345@gmail.com>
2014-09-29 11:29:06 +0100

    update branchAndTrack to detect python version and use shell for subprocess

--- branchAndTrack.py
+++ branchAndTrack.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+if sys.version_info < (3, 0):
+    sys.stdout.write("Sorry, requires Python 3.x - you have " + sys.version)
+    sys.exit(1)
+
 import subprocess
 
 #Get remotes string from git
-remotesString = subprocess.check_output('git branch -r', universal_newlines=True)
+remotesString = subprocess.check_output('git branch -r', shell=True, universal_newlines=True)
 
 #split into lines
 remotesWithSpaces = remotesString.split('\n')
@@ -17,5 +24,5 @@ for remote in remotes:
     if len(localList) > 1:
         local = localList[1]
         if not local.startswith(('HEAD', 'master')):
-            subprocess.check_call('git branch --track ' + local + ' origin/' + local)
+            subprocess.check_call('git branch --track ' + local + ' origin/' + local, shell=True)
 
