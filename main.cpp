981356a1f32938054e91b0d39ee25f36167ee5e9
shearer12345 <shearer12345@gmail.com>
2014-10-05 17:56:16 +0100

    break main up in to functions add better comments

--- main.cpp
+++ main.cpp
@@ -4,71 +4,117 @@
 
 using namespace std;
 
-int main( int argc, char* args[] )
-{
+//global variables
 
+SDL_Window *win; //pointer to the SDL_Window
+SDL_Renderer *ren; //pointer to the SDL_Renderer
+
+SDL_Texture *tex; //pointer to an SDL_Texture
+
+void initialise()
+{
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
 		cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+		exit(1);
 	}
 	cout << "SDL initialised OK!\n";
-    
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+}
+
+void createWindow()
+{
+	win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == nullptr)
 	{
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return 1;
+		exit(1);
 	}
 	cout << "SDL CreatedWindow OK!\n";
-    
-	// set the opengl context version
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); //core profile
+}
 
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+void setGLAttributes()
+{
+	// set the opengl context version
+	int major = 3;
+	int minor = 3;
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, major);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, minor);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); //core profile
+	cout << "Set OpenGL context to version " << major << "." << minor << " OK!\n";
+}
+
+void createContext()
+{
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr){
 		SDL_DestroyWindow(win);
 		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return 1;
+		exit(1);
 	}
+	cout << "Created OpenGL context OK!\n";
+}
 
+void loadAssets()
+{
 	SDL_Surface *bmp = SDL_LoadBMP("assets/hello.bmp");
 	if (bmp == nullptr){
 		SDL_DestroyRenderer(ren);
 		SDL_DestroyWindow(win);
 		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return 1;
+		exit(1);
 	}
 
-
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	tex = SDL_CreateTextureFromSurface(ren, bmp);
 	SDL_FreeSurface(bmp);
 	if (tex == nullptr){
 		SDL_DestroyRenderer(ren);
 		SDL_DestroyWindow(win);
 		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-		return 1;
+		exit(1);
 	}
+	cout << "Loaded Assets OK!\n";
+}
+
+void cleanUp()
+{
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	cout << "Cleaning up OK!\n";
+}
+
+int main( int argc, char* args[] )
+{
+
+	//setup
+	//- do just once
+	initialise();
+	createWindow();
+	setGLAttributes();
+	createContext();
+
+	//load stuff from files
+	//- usually do just once
+	loadAssets();
+	
+	
+	//clear framebuffer, render, and present the framebuffer to the display
+	//- usually loop forever, but not in this example
 
 	SDL_RenderClear(ren);
 	SDL_RenderCopy(ren, tex, NULL, NULL);
 	SDL_RenderPresent(ren);
 
-
+	//not looping, so delay so we can see what it does
 	SDL_Delay(2000); //Wait for 2 seconds before exiting
 
 	
-	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
+	//cleanup and exit
+	cleanUp();
 	SDL_Quit();
-
-	
 
 	return 0;
 }
