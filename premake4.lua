224e690047ab93eec93f2a70daddd131cd9bb700
shearer12345 <shearer12345@gmail.com>
2014-10-19 19:08:29 +0100

    make build on linux

--- premake4.lua
+++ premake4.lua
@@ -10,18 +10,23 @@ solution "graphicsExamplesInGitBranches"
    project (branch)  -- this project is called the name of the current git branch
       kind "ConsoleApp"
       language "C++"
-      buildoptions "-std=c++11" --http://industriousone.com/topic/xcode4-c11-build-option
+      configuration "linux"
+          buildoptions "-std=c++11" --http://industriousone.com/topic/xcode4-c11-build-option
 
       files { "**.h", "**.cpp" }
 
       -- where are header files?
       includedirs {
                     "./graphics_dependencies/SDL2-2.0.3/include",
-                    "./graphics_dependencies/glew-1.11.0/include"      
+                    "/usr/include/SDL2",
+                    "./graphics_dependencies/glew-1.11.0/include",
                   }
 
       -- what libraries need linking to
-      links { "SDL2", "SDL2main", "opengl32", "glew32" }
+      configuration "windows"
+          links { "SDL2", "SDL2main", "opengl32", "glew32" }
+      configuration "linux"
+          links { "SDL2", "SDL2main", "GL", "GLEW" }
 
       -- where are libraries? 
       libdirs { 
@@ -36,4 +41,5 @@ solution "graphicsExamplesInGitBranches"
  
       configuration "Release"
          defines { "NDEBUG" }
-         flags { "Optimize" }    
+         flags { "Optimize" }
+         
