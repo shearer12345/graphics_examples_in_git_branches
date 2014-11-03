cc70046cb962fc8acb10ae4b9026d08a6bb4b6a0
shearer12345 <shearer12345@gmail.com>
2014-11-03 21:13:47 +0000

    make demo of better timing

--- main.cpp
+++ main.cpp
@@ -312,22 +312,24 @@ int main( int argc, char* args[] )
 	//- usually do just once
 	loadAssets();
 
-
+    int ticksLast = SDL_GetTicks();
 	while (!done && (SDL_GetTicks() < 5000000)) //LOOP FROM HERE, for 2000ms (or if done flag is set)
 		//WARNING: SDL_GetTicks is only accurate to milliseconds, use SDL_GetPerformanceCounter and SDL_GetPerformanceFrequency for higher accuracy
 	{
 		//GET INPUT HERE - PLACEHOLDER
+                int delta = SDL_GetTicks() - ticksLast;
+                ticksLast = SDL_GetTicks();
 
-		updateSimulation(0.02); //call update simulation with an amount of time to simulate for (in seconds)
+		updateSimulation(delta * 0.001f); //call update simulation with an amount of time to simulate for (in seconds)
 		  //WARNING - we are always updating by a constant amount of time. This should be tied to how long has elapsed
 		    // see, for example, http://headerphile.blogspot.co.uk/2014/07/part-9-no-more-delays.html
-        glViewport(0,0,600,600);
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+                glViewport(0,0,600,600);
+                glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		render(); //RENDER HERE - PLACEHOLDER
 
-		SDL_GL_SwapWindow(win);; //present the frame buffer to the display (swapBuffers)
+                SDL_GL_SwapWindow(win);; //present the frame buffer to the display (swapBuffers)
 
 	} //LOOP TO HERE
 
